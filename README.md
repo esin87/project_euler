075074bbedfd711fe0f835eb03ce8bc549fdaa2e
esin87 <esin.saribudak@gmail.com>
2020-02-20 17:38:45 -0600

    update readme and complete problem 1

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project Euler Challenges
+
+A place to keep track of coding challenges for [Project Euler](https://projecteuler.net/).
---
+++ p001.js
@@ -0,0 +1,25 @@
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+const multiplesOf3And5 = () => {
+	let multiples = [];
+	for (let i = 0; i < 1000; i++) {
+		if (i % 3 === 0 || i % 5 === 0) {
+			multiples.push(i);
+		}
+	}
+	return multiples.reduce((a, b) => a + b);
+};
+
+// console.log(multiplesOf3And5());
+//Answer: 233168
+
+const sumOfThreesAndFives = () => {
+	return [...Array(1000).keys()]
+		.filter(x => x % 3 === 0 || x % 5 === 0)
+		.reduce((a, b) => a + b);
+};
+
+console.log(sumOfThreesAndFives());
+//Answer: 233168
