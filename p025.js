36bd89fac4d1d3ae9b6a22d98132da1c52889e6b
esin87 <esin.saribudak@gmail.com>
2021-02-16 20:03:55 -0600

    Complete problem 25

---
+++ p025.js
@@ -0,0 +1,40 @@
+// 1000-digit Fibonacci number
+// Show HTML problem content
+// Problem 25
+// The Fibonacci sequence is defined by the recurrence relation:
+
+// Fn = Fn−1 + Fn−2, where F1 = 1 and F2 = 1.
+// Hence the first 12 terms will be:
+
+// F1 = 1
+// F2 = 1
+// F3 = 2
+// F4 = 3
+// F5 = 5
+// F6 = 8
+// F7 = 13
+// F8 = 21
+// F9 = 34
+// F10 = 55
+// F11 = 89
+// F12 = 144
+// The 12th term, F12, is the first term to contain three digits.
+
+// What is the index of the first term in the Fibonacci sequence to contain 1000 digits?
+
+const getNthDigitFib = (n) => {
+	let prevFib = BigInt(0);
+	let currentFib = BigInt(1);
+	let index = 1;
+	while (currentFib.toString().length < n) {
+		index++;
+		let temp = currentFib;
+		currentFib = currentFib + prevFib;
+		prevFib = temp;
+	}
+
+	return index;
+};
+
+// console.log(getNthDigitFib(3)); //12
+console.log(getNthDigitFib(1000)); // 4782
