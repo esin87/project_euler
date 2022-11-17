14e0a8bf0f9d8b5b5538d5cb9c8ac39d4566a63e
esin87 <esin.saribudak@gmail.com>
2022-11-17 16:58:31 -0600

    Complete problem 52

---
+++ 051-060/p052.js
@@ -0,0 +1,42 @@
+// Permuted multiples
+
+// Problem 52
+// It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a different order.
+
+// Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+
+function getSmallestPermutedMultiple() {
+	let i = 1;
+	while (i < 142858) {
+		const multiplesArr = generateMultiples(i);
+		const success = checkIfPalindromes(multiplesArr);
+		if (success) {
+			return multiplesArr[0];
+		} else {
+			i++;
+		}
+	}
+}
+
+function generateMultiples(i) {
+	const nums = [i];
+	for (let j = 2; j <= 6; j++) {
+		nums.push(i * j);
+	}
+	return nums;
+}
+
+function checkIfPalindromes(arr) {
+	const sortedDigits = arr[0].toString().split('').sort();
+	const numStr = sortedDigits.join('');
+	for (let i = 1; i < arr.length; i++) {
+		const temp = arr[i].toString().split('').sort();
+		const tempStr = temp.join('');
+		if (tempStr !== numStr) {
+			return false;
+		}
+	}
+	return true;
+}
+
+console.log(getSmallestPermutedMultiple());
